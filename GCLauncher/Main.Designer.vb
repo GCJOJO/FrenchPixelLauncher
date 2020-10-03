7c1160a9878a92e23651d85ddc0267964a0288de
GCJOJO <39382270+GCJOJO@users.noreply.github.com>
2020-10-03 14:23:19 +0200

    Create Project

---
+++ GCLauncher/Main.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.COZBanner = New System.Windows.Forms.PictureBox()
+        Me.COZBtn = New System.Windows.Forms.Button()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.DownloadPath = New System.Windows.Forms.FolderBrowserDialog()
+        Me.COZPath = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Btn_COZ_Options = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.UpdatedTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.BTN_DungeonEditor = New System.Windows.Forms.Button()
+        Me.OptionDE = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DownloadDungeonEditorSrc = New System.Windows.Forms.Button()
+        Me.DownloadCOZSrc = New System.Windows.Forms.Button()
+        Me.COZDownloadServer = New System.Windows.Forms.Button()
+        CType(Me.COZBanner, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'COZBanner
+        '
+        Me.COZBanner.Image = CType(resources.GetObject("COZBanner.Image"), System.Drawing.Image)
+        Me.COZBanner.Location = New System.Drawing.Point(6, 27)
+        Me.COZBanner.Name = "COZBanner"
+        Me.COZBanner.Size = New System.Drawing.Size(256, 226)
+        Me.COZBanner.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.COZBanner.TabIndex = 0
+        Me.COZBanner.TabStop = False
+        '
+        'COZBtn
+        '
+        Me.COZBtn.Location = New System.Drawing.Point(6, 216)
+        Me.COZBtn.Name = "COZBtn"
+        Me.COZBtn.Size = New System.Drawing.Size(218, 37)
+        Me.COZBtn.TabIndex = 1
+        Me.COZBtn.Text = "Télécharger"
+        Me.COZBtn.UseVisualStyleBackColor = True
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "Test Image")
+        Me.ImageList1.Images.SetKeyName(1, "Icon.png")
+        '
+        'COZPath
+        '
+        Me.COZPath.AutoSize = True
+        Me.COZPath.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.COZPath.Location = New System.Drawing.Point(116, 256)
+        Me.COZPath.Name = "COZPath"
+        Me.COZPath.Size = New System.Drawing.Size(29, 13)
+        Me.COZPath.TabIndex = 2
+        Me.COZPath.Text = "Path"
+        Me.COZPath.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.COZPath.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.Label1.Location = New System.Drawing.Point(1, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(261, 26)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Project Clash Of Zombies"
+        '
+        'Btn_COZ_Options
+        '
+        Me.Btn_COZ_Options.Location = New System.Drawing.Point(221, 216)
+        Me.Btn_COZ_Options.Name = "Btn_COZ_Options"
+        Me.Btn_COZ_Options.Size = New System.Drawing.Size(41, 37)
+        Me.Btn_COZ_Options.TabIndex = 4
+        Me.Btn_COZ_Options.Text = "..."
+        Me.Btn_COZ_Options.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'UpdatedTimer
+        '
+        Me.UpdatedTimer.Enabled = True
+        Me.UpdatedTimer.Interval = 300000
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(405, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(218, 226)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'BTN_DungeonEditor
+        '
+        Me.BTN_DungeonEditor.Location = New System.Drawing.Point(380, 216)
+        Me.BTN_DungeonEditor.Name = "BTN_DungeonEditor"
+        Me.BTN_DungeonEditor.Size = New System.Drawing.Size(218, 37)
+        Me.BTN_DungeonEditor.TabIndex = 6
+        Me.BTN_DungeonEditor.Text = "Télécharger"
+        Me.BTN_DungeonEditor.UseVisualStyleBackColor = True
+        '
+        'OptionDE
+        '
+        Me.OptionDE.Location = New System.Drawing.Point(595, 216)
+        Me.OptionDE.Name = "OptionDE"
+        Me.OptionDE.Size = New System.Drawing.Size(41, 37)
+        Me.OptionDE.TabIndex = 7
+        Me.OptionDE.Text = "..."
+        Me.OptionDE.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.Label2.Location = New System.Drawing.Point(435, -2)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(163, 26)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Dungeon Editor"
+        '
+        'DownloadDungeonEditorSrc
+        '
+        Me.DownloadDungeonEditorSrc.Enabled = False
+        Me.DownloadDungeonEditorSrc.Location = New System.Drawing.Point(405, 256)
+        Me.DownloadDungeonEditorSrc.Name = "DownloadDungeonEditorSrc"
+        Me.DownloadDungeonEditorSrc.Size = New System.Drawing.Size(231, 32)
+        Me.DownloadDungeonEditorSrc.TabIndex = 9
+        Me.DownloadDungeonEditorSrc.Text = "Télécharger le code source"
+        Me.DownloadDungeonEditorSrc.UseVisualStyleBackColor = True
+        Me.DownloadDungeonEditorSrc.Visible = False
+        '
+        'DownloadCOZSrc
+        '
+        Me.DownloadCOZSrc.Enabled = False
+        Me.DownloadCOZSrc.Location = New System.Drawing.Point(31, 256)
+        Me.DownloadCOZSrc.Name = "DownloadCOZSrc"
+        Me.DownloadCOZSrc.Size = New System.Drawing.Size(231, 32)
+        Me.DownloadCOZSrc.TabIndex = 10
+        Me.DownloadCOZSrc.Text = "Télécharger le code source"
+        Me.DownloadCOZSrc.UseVisualStyleBackColor = True
+        Me.DownloadCOZSrc.Visible = False
+        '
+        'COZDownloadServer
+        '
+        Me.COZDownloadServer.Enabled = False
+        Me.COZDownloadServer.Location = New System.Drawing.Point(31, 294)
+        Me.COZDownloadServer.Name = "COZDownloadServer"
+        Me.COZDownloadServer.Size = New System.Drawing.Size(231, 32)
+        Me.COZDownloadServer.TabIndex = 11
+        Me.COZDownloadServer.Text = "Télécharger le server"
+        Me.COZDownloadServer.UseVisualStyleBackColor = True
+        Me.COZDownloadServer.Visible = False
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(25, Byte), Integer), CType(CType(25, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.COZDownloadServer)
+        Me.Controls.Add(Me.DownloadCOZSrc)
+        Me.Controls.Add(Me.DownloadDungeonEditorSrc)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BTN_DungeonEditor)
+        Me.Controls.Add(Me.OptionDE)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.COZBtn)
+        Me.Controls.Add(Me.Btn_COZ_Options)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.COZPath)
+        Me.Controls.Add(Me.COZBanner)
+        Me.Name = "Main"
+        Me.Text = "FrenchPixelLauncher"
+        CType(Me.COZBanner, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents COZBanner As PictureBox
+    Friend WithEvents COZBtn As Button
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents DownloadPath As FolderBrowserDialog
+    Friend WithEvents COZPath As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Btn_COZ_Options As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents UpdatedTimer As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents BTN_DungeonEditor As Button
+    Friend WithEvents OptionDE As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DownloadDungeonEditorSrc As Button
+    Friend WithEvents DownloadCOZSrc As Button
+    Friend WithEvents COZDownloadServer As Button
+End Class
---
+++ GCLauncher/Main.vb
@@ -0,0 +1,275 @@
+﻿Public Structure Game
+    Dim name As String
+    Dim id As Integer
+    Dim FullName As String
+    Dim downloadURL As String
+    Dim downloadSRCUrl As String
+End Structure
+
+
+
+Public Class Main
+
+    Dim COZDownloaded As Boolean
+    Dim DEDownloaded As Boolean
+    'Dim CanAskUpdate As Boolean = True
+
+    Dim COZ As Game
+    Dim DungeonEditor As Game
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        COZ.name = "COZ"
+        COZ.FullName = "ClashOfZombies"
+        COZ.id = 0
+        COZ.downloadURL = "https://github.com/GCJOJO/COZ/releases/latest/download/COZWindows.zip"
+        COZ.downloadSRCUrl = "https://github.com/GCJOJO/COZ/archive/master.zip"
+
+        DungeonEditor.name = "DungeonEditor"
+        DungeonEditor.FullName = "DungeonEditor"
+        DungeonEditor.id = 1
+        DungeonEditor.downloadURL = "https://github.com/GCJOJO/DungeonEditor/releases/latest/download/DungeonEditorWindows.zip"
+        DungeonEditor.downloadSRCUrl = "https://github.com/GCJOJO/DungeonEditor/archive/master.zip"
+
+        Try
+            Dim COZPathReader As String
+            COZPathReader = My.Computer.FileSystem.ReadAllText(Application.StartupPath & "\" & COZ.name & "Path.txt")
+            'MsgBox(COZPathReader, MsgBoxStyle.MsgBoxHelp)
+
+            Process.Start(COZPathReader).Kill()
+            COZDownloaded = True
+            COZBtn.Text = "Lancer"
+            'COZPath.Visible = True
+            COZPath.Text = COZPathReader
+            COZBtn.Width = 218
+            'checkCOZUpdated()
+        Catch ex As Exception
+            COZDownloaded = False
+            COZBtn.Text = "Télécharger"
+            COZPath.Visible = False
+            COZBtn.Width = 256
+            Process.Start("cmd.exe", "/c del COZPath.txt")
+
+            'MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+
+        Try
+            Dim DungeonEditorPathReader As String
+            DungeonEditorPathReader = My.Computer.FileSystem.ReadAllText(Application.StartupPath & "\" & DungeonEditor.name & "Path.txt")
+            'MsgBox(COZPathReader, MsgBoxStyle.MsgBoxHelp)
+
+            Process.Start(DungeonEditorPathReader).Kill()
+            DEDownloaded = True
+            BTN_DungeonEditor.Text = "Lancer"
+            BTN_DungeonEditor.Width = 218
+        Catch ex As Exception
+            DEDownloaded = False
+            BTN_DungeonEditor.Text = "Télécharger"
+            BTN_DungeonEditor.Width = 256
+            Process.Start("cmd.exe", "/c del DungeonEditorPath.txt")
+
+            'MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+
+    Private Sub COZBtn_Click(sender As Object, e As EventArgs) Handles COZBtn.Click
+        '    Try
+        '        Shell(Application.ExecutablePath & "\Games\COZ\Binaries\COZ.exe")
+        '    Catch ex As Exception
+        '        MsgBox(ex.Message, MsgBoxStyle.Critical)
+        '    End Try
+        '    If COZDownloaded Then
+        '        Dim COZPathReader As String
+        '        COZPathReader = My.Computer.FileSystem.ReadAllText(Application.StartupPath & "\COZPath.txt")
+        '        Process.Start(COZPathReader)
+        '    Else
+        '        If COZDownloadPath.ShowDialog() = DialogResult.OK Then
+        '            Shell("cmd.exe /c" & "curl file:///D:/repos/GCLauncher/GCLauncher/DownloadCOZ.exe -o " & COZDownloadPath.SelectedPath & "\COZ.Downloader.exe", AppWinStyle.Hide)
+        '            Shell("cmd.exe /c" & "echo " & COZDownloadPath.SelectedPath & "\ClashOfZombies\COZ.exe> COZPath.txt", AppWinStyle.Hide)
+
+        '            Dim COZPathWriter As System.IO.StreamWriter
+        '            COZPathWriter = My.Computer.FileSystem.OpenTextFileWriter(Application.StartupPath & "\COZPath.txt", True)
+        '            COZPathWriter.Write(COZDownloadPath.SelectedPath & "\ClashOfZombies\COZ.exe")
+        '            COZPathWriter.Close()
+
+        '            Process.Start("cmd.exe", "/c cd " & COZDownloadPath.SelectedPath & "\ & curl -L https://github.com/GCJOJO/COZ/releases/latest/download/COZWindows.zip -o" & COZDownloadPath.SelectedPath & "\COZ.zip & tar -xf " & COZDownloadPath.SelectedPath & "\COZ.zip -C" & COZDownloadPath.SelectedPath & "& exit")
+        '            Shell("cmd.exe /c" & COZDownloadPath.SelectedPath & "\COZ.Downloader.exe", AppWinStyle.hide)
+
+        '            Download Game
+        '            Shell("cmd.exe /k " & "curl file:///D:/repos/COZLauncher/COZ.zip -o " & COZDownloadPath.SelectedPath & "\COZ.zip")
+
+        '            COZPath.Text = COZDownloadPath.SelectedPath
+        '            COZPath.Visible = True
+        '            COZDownloaded = True
+        '            COZBtn.Text = "Lancer"
+        '            COZBtn.Width = 218
+        '            If checkCOZUpdated() = 2 Then
+        '            ElseIf checkCOZUpdated() = 1 Then
+        '            ElseIf checkCOZUpdated() = 0 Then
+        '                COZDownloaded = False
+        '                COZBtn.Text = "Mettre à jour"
+        '            End If
+        '        End If
+        '    End If
+        If COZDownloaded Then
+            LaunchGame(COZ)
+        Else
+            DownloadGame(COZ)
+            COZDownloaded = True
+            COZBtn.Text = "Lancer"
+            COZBtn.Width = 218
+        End If
+    End Sub
+
+    Private Sub BTN_DungeonEditor_Click(sender As Object, e As EventArgs) Handles BTN_DungeonEditor.Click
+        If DEDownloaded Then
+            LaunchGame(DungeonEditor)
+        Else
+            DownloadGame(DungeonEditor)
+            DEDownloaded = True
+            BTN_DungeonEditor.Text = "Lancer"
+            BTN_DungeonEditor.Width = 218
+        End If
+    End Sub
+
+    Public Function DownloadGame(GameRef As Game) As Boolean
+        Dim url As String = GameRef.downloadURL
+        If DownloadPath.ShowDialog() = DialogResult.OK Then
+
+            Dim PathWriter As System.IO.StreamWriter
+            PathWriter = My.Computer.FileSystem.OpenTextFileWriter(Application.StartupPath & "\" & GameRef.name & "Path.txt", True)
+            PathWriter.Write(DownloadPath.SelectedPath & "\" & GameRef.FullName & "\" & GameRef.name & ".exe")
+            PathWriter.Close()
+
+            Process.Start("cmd.exe", "/c title French Pixel Download - " & GameRef.FullName & "& echo [1m[32m[4mBienvenue dans l'interface de téléchargement du French Pixel Launcheur (oui on utilise cmd.exe)[0m & cd " & DownloadPath.SelectedPath & "\ & curl -L " & url & " -o" & DownloadPath.SelectedPath & "\" & GameRef.name & ".zip & tar -xf " & DownloadPath.SelectedPath & "\" & GameRef.name & ".zip -C" & DownloadPath.SelectedPath & " & ren " & DownloadPath.SelectedPath & "\WindowsNoEditor " & GameRef.FullName & " & exit")
+            Return True
+        End If
+        Return False
+    End Function
+
+    Public Function LaunchGame(GameRef As Game) As Boolean
+        Dim GamePathReader As String
+        GamePathReader = My.Computer.FileSystem.ReadAllText(Application.StartupPath & "\" & GameRef.name & "Path.txt")
+        Process.Start(GamePathReader)
+        Return True
+    End Function
+
+    Public Function DownloadGameSource(GameRef As Game) As Boolean
+        Dim url As String = GameRef.downloadSRCUrl
+        If DownloadPath.ShowDialog() = DialogResult.OK Then
+            Process.Start("cmd.exe", "/c title French Pixel Download - " & GameRef.FullName & " (Game source) & echo [1m[32m[4mBienvenue dans l'interface de téléchargement du French Pixel Launcheur (oui on utilise cmd.exe)[0m & cd " & DownloadPath.SelectedPath & "\ & curl -L " & url & " -o" & DownloadPath.SelectedPath & "\" & GameRef.name & "_src.zip & tar -xf " & DownloadPath.SelectedPath & "\" & GameRef.name & ".zip -C" & DownloadPath.SelectedPath & " & ren " & DownloadPath.SelectedPath & "\WindowsNoEditor " & GameRef.FullName & "Source & exit")
+            Return True
+        End If
+        Return False
+    End Function
+
+
+    Private Sub UpdatedTimer_Tick(sender As Object, e As EventArgs) Handles UpdatedTimer.Tick
+        'checkCOZUpdated()
+    End Sub
+
+    Dim isDEOptions As Boolean
+
+    Private Sub OptionDE_Click(sender As Object, e As EventArgs) Handles OptionDE.Click
+        If isDEOptions Then
+            isDEOptions = False
+            DownloadDungeonEditorSrc.Enabled = False
+            DownloadDungeonEditorSrc.Visible = False
+        Else
+            isDEOptions = True
+            DownloadDungeonEditorSrc.Enabled = True
+            DownloadDungeonEditorSrc.Visible = True
+        End If
+    End Sub
+
+    Dim isCOZOptions As Boolean
+    Private Sub Btn_COZ_Options_Click(sender As Object, e As EventArgs) Handles Btn_COZ_Options.Click
+        If isCOZOptions Then
+            isCOZOptions = False
+            DownloadCOZSrc.Enabled = False
+            DownloadCOZSrc.Visible = False
+            COZDownloadServer.Visible = False
+            COZDownloadServer.Enabled = False
+        Else
+            isCOZOptions = True
+            DownloadCOZSrc.Enabled = True
+            DownloadCOZSrc.Visible = True
+            COZDownloadServer.Visible = True
+            COZDownloadServer.Enabled = True
+        End If
+    End Sub
+
+    Private Sub BTN_COZ_Src(sender As Object, e As EventArgs) Handles DownloadCOZSrc.Click
+        DownloadGameSource(COZ)
+    End Sub
+
+    Private Sub BTN_DE_Src(sender As Object, e As EventArgs) Handles DownloadDungeonEditorSrc.Click
+        DownloadGameSource(DungeonEditor)
+    End Sub
+
+    'Public Function checkCOZUpdated() As Integer
+    '    If COZDownloaded Then
+    '        If CanAskUpdate Then
+    '            CanAskUpdate = False
+    '            'Shell("cmd.exe /c curl -L https://github.com/GCJOJO/COZ/releases/latest/download/latest.json -o coz.latest.json", AppWinStyle.Hide)
+    '            'Dim newLatest As FileInfo = New FileInfo(Application.StartupPath & "\coz.latest.json")
+    '            'Dim oldLatest As FileInfo = New FileInfo(COZPath.Text & "\latest.json")
+
+    '            'If FilesContentsAreEqual(newLatest, oldLatest) Then
+    '            'End If
+    '            'If MsgBox("Mettre à jour COZ?", MsgBoxStyle.YesNo) = DialogResult.OK Then
+    '            Return 0
+    '            'End If
+    '        End If
+    '        Return 1
+    '    End If
+    '    Return 2
+    'End Function
+
+    'CTRL + K + C = Comment
+    'CTRL + K + U = Uncomment
+
+    'Public Shared Function filescontentsareequal(fileinfo1 As FileInfo, fileinfo2 As FileInfo) As Boolean
+    '    Dim result As Boolean
+
+    '    If fileinfo1.Length <> fileinfo2.Length Then
+    '        Return False
+    '    End If
+    '    Using file1 = fileinfo1.OpenRead()
+    '        Using file2 = fileinfo2.OpenRead()
+    '            result = streamscontentsareequal(file1, file2)
+    '        End Using
+    '    End Using
+    '    Return result
+    'End Function
+
+    'Public Shared Function streamscontentsareequal(stream1 As Stream, stream2 As Stream) As Boolean
+    '    Const buffersize As Integer = 2048 * 2
+    '    Dim buffer1 = New Byte(buffersize - 1) {}
+    '    Dim buffer2 = New Byte(buffersize - 1) {}
+
+    '    While True
+    '        Dim count1 = stream1.Read(buffer1, 0, buffersize)
+    '        Dim count2 = stream2.Read(buffer2, 0, buffersize)
+
+    '        If count1 <> count2 Then
+    '            Return False
+    '        End If
+
+    '        If count1 = 0 Then
+    '            Return True
+    '        End If
+
+    '        Dim iterations = CInt(Math.Ceiling(CDbl(count1) / sizeof(Int64)))
+    '        For i As var = 0 To iterations - 1
+    '            If BitConverter.ToInt64(buffer1, i * sizeof(Int64)) <> BitConverter.ToInt64(buffer2, i * sizeof(Int64)) Then
+    '                Return False
+    '            End If
+    '        Next
+    '    End While
+    'End Function
+
+End Class
+'Shell("cmd.exe /c " + COZDownloadPath.SelectedPath, AppWinStyle.Hide)
+'COZPath.Text = COZDownloadPath.SelectedPath
